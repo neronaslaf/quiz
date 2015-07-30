ac132f5095f006f2ac2545424229149e44bacf78
neron <neronaslaf@gmail.com>
2015-07-30 21:53:35 +0200

    Validacion

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []})
 });
 
 // Autoload de comandos con :quizId
@@ -17,9 +17,6 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
-
-router.get('/author', function(req, res) {
-  res.render('author', { title: 'Créditos' });
-});
+router.get('/author',                      quizController.author);
 
 module.exports = router;
